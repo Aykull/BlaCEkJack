760b6934a1bddf9bb54c4e109fa85952b80d2f7e
Lucy M Golcher <luciamongeg@gmail.com>
2019-10-17 23:34:11 -0600

    Se tienen las imagenes de cartas

--- GUI.rkt
+++ GUI.rkt
@@ -1,16 +1,25 @@
 #lang racket/gui
+(require net/url)
+
 (provide mostrarGUI)
 
 (define frame (new frame%
                    [label "BlaCEkJack"]
                    [width 800]
                    [height 800]))
-(new canvas% [parent frame]
-             [paint-callback
-              (lambda (canvas dc)
-                (send dc set-scale 3 3)
-                (send dc set-text-foreground "blue")
-                (send dc draw-text "Aykull" 0 0))])
+
+(define bm (make-object bitmap% (get-pure-port
+                                 (string->url "http://imagenes.4ever.eu/data/download/abstractos/fondo-verde-217279.jpg?no-logo"))))
 
 (define (mostrarGUI)
-  (send frame show #t))
+  (send frame show #t) )
+
+(define mycanvas%
+  (class canvas%
+    (super-new)
+    (inherit get-dc)
+    (define/override (on-paint)
+      (let ([my-dc (get-dc)])
+        (send my-dc draw-bitmap bm 0 0)))))
+
+(define c (new mycanvas% [parent frame]))
