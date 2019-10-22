5cce4ef02d4950f9d5c420398657d035e6f46aa3
josuemata800 <josuemata800@gmail.com>
2019-10-22 12:16:57 -0600

    Interfaz gráfica básica y funcional, con funciones de prueba

--- GUI.rkt
+++ GUI.rkt
@@ -1,109 +1,292 @@
-#lang racket/gui
-(require racket/draw)
+#lang racket
 
-(require "Logica.rkt")
-
-;Se crea el marco en donde se realiza el juego
-(define frame (new frame%
-                   [label "BlaCEkJack"]
-                   [width 800]
-                   [height 800]
-                   [style (list 'no-resize-border)]))
-                   
-
-;Se cargan todas las imagenes del deck
-(define bg (read-bitmap "Fondo.png"))
-
-(define back (read-bitmap "Back.png"))
-
-(define Ah (read-bitmap "Ah.png"))
-(define 2h (read-bitmap "2h.png"))
-(define 3h (read-bitmap "3h.png"))
-(define 4h (read-bitmap "4h.png"))
-(define 5h (read-bitmap "5h.png"))
-(define 6h (read-bitmap "6h.png"))
-(define 7h (read-bitmap "7h.png"))
-(define 8h (read-bitmap "8h.png"))
-(define 9h (read-bitmap "9h.png"))
-(define 10h (read-bitmap "10h.png"))
-(define Jh (read-bitmap "Jh.png"))
-(define Qh (read-bitmap "Qh.png"))
-(define Kh (read-bitmap "Kh.png"))
-
-(define Ac (read-bitmap "Ac.png"))
-(define 2c (read-bitmap "2c.png"))
-(define 3c (read-bitmap "3c.png"))
-(define 4c (read-bitmap "4c.png"))
-(define 5c (read-bitmap "5c.png"))
-(define 6c (read-bitmap "6c.png"))
-(define 7c (read-bitmap "7c.png"))
-(define 8c (read-bitmap "8c.png"))
-(define 9c (read-bitmap "9c.png"))
-(define 10c (read-bitmap "10c.png"))
-(define Jc (read-bitmap "Jc.png"))
-(define Qc (read-bitmap "Qc.png"))
-(define Kc (read-bitmap "Kc.png"))
-
-(define As (read-bitmap "As.png"))
-(define 2s (read-bitmap "2s.png"))
-(define 3s (read-bitmap "3s.png"))
-(define 4s (read-bitmap "4s.png"))
-(define 5s (read-bitmap "5s.png"))
-(define 6s (read-bitmap "6s.png"))
-(define 7s (read-bitmap "7s.png"))
-(define 8s (read-bitmap "8s.png"))
-(define 9s (read-bitmap "9s.png"))
-(define 10s (read-bitmap "10s.png"))
-(define Js (read-bitmap "Js.png"))
-(define Qs (read-bitmap "Qs.png"))
-(define Ks (read-bitmap "Ks.png"))
-
-(define Ad (read-bitmap "Ad.png"))
-(define 2d (read-bitmap "2d.png"))
-(define 3d (read-bitmap "3d.png"))
-(define 4d (read-bitmap "4d.png"))
-(define 5d (read-bitmap "5d.png"))
-(define 6d (read-bitmap "6d.png"))
-(define 7d (read-bitmap "7d.png"))
-(define 8d (read-bitmap "8d.png"))
-(define 9d (read-bitmap "9d.png"))
-(define 10d (read-bitmap "10d.png"))
-(define Jd (read-bitmap "Jd.png"))
-(define Qd (read-bitmap "Qd.png"))
-(define Kd (read-bitmap "Kd.png"))
+(require games/cards racket/gui racket/class racket/unit)
 
 
+;; Definición del número de Decks
+(define DECK-COUNT 1)
 
-;Funciones para dibujar las imagenes como bitmaps
-(define bitmap-canvas%
-  (class canvas%
-    (init-field [bitmap #f])
-    (inherit get-dc)
-    (define/override (on-paint)
-      (send (get-dc) draw-bitmap bitmap 0 0))
-    (super-new)))
-
-;Se define un canvas para pintar en el frame y que se muestre el fondo
-(define mycanvas (new bitmap-canvas% [parent frame] [bitmap bg]))
-
-(define msg (new message% [parent frame]
-                          [label "No events so far..."]))
+;; Constantes que definen los límites de las regiones
+(define MARGIN 10)
+(define SUBMARGIN 10)
+(define LABEL-H 15)
 
 
-(new button% [parent frame]
-             [label "Pedir Carta"]
-             ; Callback procedure for a button click:
-             [callback (lambda (button event)
-                         (send msg set-label "Button click 1"))])
+;; Se crea una mesa
+(define table (make-table "Blackjack" 6 3))
+(define status-pane (send table create-status-pane))
+(send table add-scribble-button status-pane
+      '(lib "games/scribblings/games.scrbl") "blackjack")
+(send table show #t)
+(send table set-double-click-action #f)
 
-(new button% [parent frame]
-             [label "Plantarme"]
-             ; Callback procedure for a button click:
-             [callback (lambda (button event)
-                         (send msg set-label "Button click 2"))])
 
-(define (mostrarGUI)
-  (send frame show #t))
+;; Retorno del ancho y el largo de la mesa
+(define w (send table table-width))
+(define h (send table table-height))
 
-(define t (make-table "Blackjack" 6 3))
+;; Construcción del DECK
+(define deck
+  (let loop ([n DECK-COUNT])
+    (if (zero? n)
+      null
+      (append (make-deck) (loop (sub1 n))))))
 
+;; Card width & height
+(define cwidth (send (car deck) card-width))
+(define cheight (send (car deck) card-height))
+
+;; Definición del tamaño de los b
+(define BUTTON-HEIGHT 16)
+(define BUTTON-WIDTH cwidth)
+
+;; Evita que el usuario mueva las cartas 
+(for-each (lambda (card) (send* card (user-can-move #f) (user-can-flip #f)))
+          deck)
+
+;; Crea regiones de juego para los jugadores
+(define deck-region
+  (make-region MARGIN MARGIN cwidth cheight #f #f))
+
+(define discard-region
+  (make-region (- w cwidth MARGIN) MARGIN cwidth cheight #f #f))
+
+(define crupier-region
+  (make-region (+ cwidth (* 2 MARGIN)) MARGIN
+               (- w (* 2 cwidth) (* 4 MARGIN)) cheight
+               #f #f))
+
+(define player1-region
+  (make-region (+ cwidth (* 2 MARGIN)) (- h (* 2 MARGIN) cheight BUTTON-HEIGHT)
+               (- w (* 2 cwidth) (* 4 MARGIN)) cheight
+               #f #f))
+
+;; In case of split, we need more regions
+(define ww (* 3/2 cwidth))
+(define player-2-region
+  (make-region MARGIN (region-y player1-region)
+               (- w ww (* 3 MARGIN)) (region-h player1-region)
+               #f #f))
+(define player-2-wait-region
+  (make-region (region-x player-2-region) (region-y player-2-region)
+               ww (region-h player-2-region)
+               #f #f))
+(define player-1-region
+  (make-region (- w MARGIN (region-w player-2-region)) (region-y player-2-region)
+               (region-w player-2-region) (region-h player-2-region)
+               #f #f))
+(define player-1-wait-region
+  (make-region (- (+ (region-x player-1-region) (region-w player-1-region)) ww)
+               (region-y player-1-region)
+               ww (region-h player-1-region)
+               #f #f))
+(define (make-border-region r)
+  (define hm (/ MARGIN 2))
+  (make-region (- (region-x r) hm) (- (region-y r) hm)
+               (+ (region-w r) MARGIN) (+ (region-h r) MARGIN)
+               "" #f))
+(define player-1-border (make-border-region player-1-region))
+(define player-2-border (make-border-region player-2-region))
+
+;; Creacion de botones
+(define (make-button title pos)
+  (make-button-region (+ (/ (- w (* 4 BUTTON-WIDTH) (* 3 MARGIN)) 2)
+                         (* pos (+ BUTTON-WIDTH MARGIN)))
+                      (- h MARGIN BUTTON-HEIGHT)
+                      BUTTON-WIDTH BUTTON-HEIGHT
+                      title void))
+(define hit-button (make-button "Hit" 1))
+(define stand-button (make-button "Stand" 2))
+
+;; Se ponen las cartas sobre la mesa
+(send table add-cards-to-region deck deck-region)
+
+;; Function to compute the normal or minimum value of a card
+(define (min-card-value c)
+  (let ([v (send c get-value)]) (if (> v 10) 10 v)))
+
+;; Function to compute the value of a hand, counting aces as 1 or 11
+;;  to get the highest total possible under 21
+(define (best-total l)
+  (let* ([ace? (lambda (is?) (lambda (c) (eq? is? (= (send c get-value) 1))))]
+         [aces (filter (ace? #t) l)]
+         [others (filter (ace? #f) l)]
+         [base (apply + (map min-card-value others))])
+    (let loop ([l aces][base base])
+      (cond [(null? l) base]
+            [(<= (+ base (* (length aces) 11)) 21)
+             (+ base (* (length aces) 11))]
+            [else (loop (cdr l) (add1 base))]))))
+
+;; Function to test whether a hand is a bust
+(define (bust? p)
+  (> (best-total p) 21))
+
+;; Funcion principal
+(let shuffle-loop ()
+  ;; Shuffle the cards, none are discarded, yet
+  (let* ([deck (shuffle-list deck 7)]
+         [discard null]
+         [deal (lambda (n)
+                 (let deal ([n n])
+                   (if (zero? n)
+                     null
+                     (let ([c (car deck)])
+                       (set! deck (cdr deck))
+                       (cons c (deal (sub1 n)))))))])
+    ;; Put the shuffled deck in place
+    (send table move-cards-to-region deck deck-region)
+    (send table stack-cards deck)
+    ;; Loop rounds over while there's enough cards in the deck
+    (let loop ()
+      (let ([p (deal 2)]
+            [p2 null]) ; in case of splitting
+        
+        ;; Move the player's cards into place and show them
+        (send table move-cards-to-region p player1-region)
+        (send table cards-face-up p)
+        
+        ;; Deal to dealer
+        (let ([d (deal 2)])
+          
+          ;; Move the dealer's cards into place and show one
+          (send table move-cards-to-region d crupier-region)
+          (send table card-face-up (car d))
+          
+          (let* (
+                 [continue (make-semaphore)]
+                 
+                 ;; Make a button in the center to show results
+                 [make-status
+                  (lambda (title continue)
+                    (let ([r (make-button-region
+                              (/ (- w (* 2 cwidth)) 2)
+                              (region-y hit-button)
+                              (* 2 cwidth) BUTTON-HEIGHT
+                              title #f)])
+                      (set-region-callback! r (lambda ()
+                                                (send table remove-region r)
+                                                (semaphore-post continue)))
+                      r))]
+                 
+                 ;; Done with hand:
+                 [done
+                  (lambda (title continue)
+                    (send table remove-region hit-button)
+                    (send table remove-region stand-button)
+                    (send table add-region (make-status title continue)))]
+                 
+                 ;; Compute winnings (not called for busts by the player)FUNCION PARA TERMINAR 
+                 [finish
+                  (lambda (p blackjack?)
+                    (let ([pt (best-total p)]
+                          [dt (best-total d)]
+                          [continue (make-semaphore)])
+                      (cond
+                        [(or (> dt 21) (> pt dt))
+                         (done (if blackjack?
+                                 "Blackjack"
+                                 "You Win")
+                               continue)]
+                        [(> dt pt)
+                         (done (if blackjack?
+                                 "Dealer Blackjack"
+                                 "You Lose")
+                               continue)]
+                        [else (done "Push" continue)])
+                      (yield continue)))]
+
+                 ;; Done with the first hand of a split
+                 [finish-split
+                  (lambda (p player-region player-wait-region player-border)
+                    (unless (bust? p)
+                      (send table move-cards-to-region p player-region)
+                      (send table add-region player-border)
+                      (finish p #f)
+                      (send table remove-region player-border)
+                      (send table move-cards-to-region p player-wait-region)))]
+
+                 ;; Player busts
+                 [bust (lambda ()
+                         (done "Bust" continue))]
+
+                 ;; Bust in one hand of a split
+                 [local-bust (lambda ()
+                               (let ([cont (make-semaphore)])
+                                 (done "Bust" cont)
+                                 (yield cont)))]
+
+                 ;; Callback for the hit button; the button's callback is
+                 ;; changed for different modes: normal, split part 1, or split
+                 ;; part 2
+                 [make-hit-callback
+                  (lambda (get-p set-p! player-region bust)
+                    (lambda ()
+                      (set-p! (append (deal 1) (get-p)))
+                      (send table stack-cards (get-p))
+                      (send table move-cards-to-region (get-p) player-region)
+                      (send table cards-face-up (get-p))
+                      ;; Check for bust
+                      (when (bust? (get-p)) (bust))))])
+
+
+            ;; Blackjack by player or dealer?
+            (if (or (= 21 (best-total p))
+                    (= 21 (best-total d)))
+              (begin
+
+                ;; Show the dealers cards...
+                (send table cards-face-up d)
+
+                ;; ... and compute the result
+                (finish p #t))
+
+              (begin
+                ;; Three basic actions are allowed:
+                (send table add-region hit-button)
+                (send table add-region stand-button)
+
+                ;; Set the callbacks for normal (unsplit) hands
+                (set-region-callback!
+                 hit-button
+                 (make-hit-callback (lambda () p)
+                                    (lambda (v) (set! p v))
+                                    player1-region
+                                    bust))
+                (set-region-callback!
+                 stand-button
+                 (lambda () (semaphore-post continue)))           
+                ;; Wait until the player is done
+                (yield continue)
+
+                ;; No more player actions; get rid of the buttons
+                (send table remove-region hit-button)
+                (send table remove-region stand-button)
+
+                ;; If all the player's hards are bust, the dealer doesn't do
+                ;; anything
+                (unless (bust? p)
+                  ;; Show the dealer's starting hand
+                  (send table card-face-up (cadr d))
+                  (let loop ()
+                    ;; Hit on 16 or lower, stand on 17 and higher
+                    (when (< (best-total d) 17)
+                      ;; Hit the dealer
+                      (set! d (append (deal 1) d))
+                      (send table stack-cards d)
+                      (send table move-cards-to-region d crupier-region)
+                      (send table cards-face-up d)
+                      (loop)))
+                  (finish p #f)
+
+                  )))
+            ;; Move all the discarded cards to the back
+            (unless (null? discard)
+              (send table card-to-back (car discard))
+              (send table stack-cards discard))
+            ;; Discard all the cards we used
+            (set! discard (append p d discard)) ;;retorna las cartas a la pila de descarte 
+            (send table cards-face-down discard) ;;pone las cartas boca abajo
+            (send table move-cards-to-region discard discard-region) ;; mueve las cartas de una zona a otra
+           
+              (loop)))))))
